0ed7592b52263641d64a53e7d77cc2cbbfbb542b
anurag.garg <anurag.garg@nagarro.com>
2020-06-03 13:29:48 +0530

    Created packages and blocks

---
+++ flowControl02/blocks.py
@@ -0,0 +1,6 @@
+# there is nothing as such blocks, in place python use indentation
+
+for i in range(1, 13):  # : depicts move to next indented line
+    print("No. {} square is {} and cube is {:4}".format(i, i ** 2, i ** 3))
+    print("#" * 10)
+print("*" * 80)
